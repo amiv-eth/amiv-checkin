595422e9672bb18cc2cee3b2941eb435969605a9
Tar-Tarus <loubia314@gmail.com>
2017-12-02 20:50:57 +0100

    Added multiple methods to e.g. set the event_id, get_userid from multiple info sources and check-in or -out a user. -(*_*)-

--- connectors/amivapi_interface.py
+++ connectors/amivapi_interface.py
@@ -34,16 +34,83 @@ class AMIV_API_Interface:
         r = requests.get(self.api_url + _filter, auth=self.auth_obj)
         return r.json()
 
-    def checkin_field(self, user_id, event_id):
-        """ Check in a user to an event by flipping the checked_in valuei """
-        r = requests.get(self.api_url + '/eventsignups/' + event_id,
+    def set_event(self, event_id):
+        """ Set the event_id for this instance of the class """
+        self.event_id = event_id
+
+    def _get_userid_from_legi(self, legi):
+        """ Fetch the user_id from a legi number """
+        _filter = '/users?where={"legi":"%s"}' % legi
+        r = requests.get(self.api_url + _filter,
+                         auth=self.auth_obj)
+        if r.status_code is 200:
+            return r.json()['_id']
+        return "Error: No user found"
+
+    def _get_userid_from_nethz(self, nethz):
+        """ Fetch the user_id from a nethz username """
+        _filter = '/users?where={"nethz":"%s"}' % nethz
+        r = requests.get(self.api_url + _filter,
+                         auth=self.auth_obj)
+        if r.status_code is 200:
+            return r.json()['_id']
+        return "Error: No user found"
+
+    def _get_userid_from_email(self, email):
+        """ Fetch the user_id from a mail adress """
+        _filter = '/users?where={"email":"%s"}' % email
+        r = requests.get(self.api_url + _filter,
+                         auth=self.auth_obj)
+        if r.status_code is 200:
+            if len(r.json().keys()) is 1:
+                return r.json()['_id']
+        return "Error: Multiple entries found or no user found"
+
+    def get_userid_from_info(self, info):
+        """ Choose the function to use to fetch the u_id """
+        if '@' in info:
+            user_id = self._get_userid_from_email(info)
+        elif info.isalpha():
+            user_id = self._get_userid_from_nethz(info)
+        else:
+            if info[0] is 'S':
+                info.replace('S', '')
+            user_id = self._get_userid_from_legi(info)
+        return user_id
+
+    def checkin_field(self, info):
+        """ Check in a user to an event by flipping the checked_in value """
+        user_id = self._get_userid_from_info(info)
+        if 'Error' in user_id:
+            return False
+        _filter = '?where={"user":"%s", "event":"%s"}' % user_id, self.event_id
+        r = requests.get(self.api_url + '/eventsignups' + _filter,
                          auth=self.auth_obj)
         if r.status_code != 200:
             return False
         etag = r.json()['_etag']
-        url = self.api_url + '/eventsignups/' + event_id
+        url = self.api_url + '/eventsignups/' + self.event_id
         header = {'If-Match': etag}
         payload = {"checked_in": "True"}
+        _filter = '?where={"user":"%s"}' % user_id
+        r = requests.patch(url + _filter,
+                           auth=self.auth_obj,
+                           headers=header,
+                           data=payload)
+        return r.status_code == 200
+
+    def checkout_field(self, info):
+        """ Check in a user to an event by flipping the checked_in value """
+        user_id = self._get_userid_from_info(info)
+        _filter = '?where={"user":"%s", "event":"%s"}' % user_id, self.event_id
+        r = requests.get(self.api_url + '/eventsignups' + _filter,
+                         auth=self.auth_obj)
+        if r.status_code != 200:
+            return False
+        etag = r.json()['_etag']
+        url = self.api_url + '/eventsignups/' + self.event_id
+        header = {'If-Match': etag}
+        payload = {"checked_in": "False"}
         _filter = '?where={"user":"%s"}' % user_id
         r = requests.patch(url + _filter,
                            auth=self.auth_obj,
