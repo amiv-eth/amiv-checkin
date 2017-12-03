ef6f0486cc9753026124d8b004fa79c6c1095030
Tar-Tarus <loubia314@gmail.com>
2017-12-03 11:52:54 +0100

    Added setting of token to use as auth. Changed load of next_events and of signups to deliver list of dict with more infos(embedded user). Changed functions to get userinfo instead of only id. Extended check-in/out to check previous state

--- app/connectors/amivapi_interface.py
+++ app/connectors/amivapi_interface.py
@@ -8,6 +8,9 @@ class AMIV_API_Interface:
         self.api_url = "https://amiv-api.ethz.ch"
         self.datetime_format = "%Y-%m-%dT%H:%M:%SZ"
 
+        self.human_string = "AMIV Events"
+        self.human_string = "amivapi_connector"
+
         self.token = ""
         self.auth_obj = ""
 
@@ -18,6 +21,14 @@ class AMIV_API_Interface:
         if r.status_code is 201:
             self.token = r.json()['token']
             self.auth_obj = requests.auth.HTTPBasicAuth(self.token, "")
+            return self.token
+        else:
+            raise Exception('Invalid username or password')
+
+    def token_login(self, token):
+        """ Log in the user by storing their session token in this class """
+        self.token = token
+        self.auth_obj = requests.auth.HTTPBasicAuth(self.token, "")
 
     def get_next_events(self):
         """ Fetch the upcoming events between today and tomorrow """
@@ -26,68 +37,97 @@ class AMIV_API_Interface:
         up_bound = up_bound.strftime(self.datetime_format)
         _range = '{"time_start":{"$gt":"'+low_bound+'","$lt":"'+up_bound+'"}}'
         r = requests.get(self.api_url + '/events?where=' + _range)
-        return r.json()
+        if min(r.json()['total'], r.json()['max_results']) is 0:
+            raise Exception("No Events found in the next 100 days")
+        response = list()
+        for key, event in r.json().iteritems():
+            response.append({
+                             '_id': event['_id'],
+                             'title': event['title'],
+                             'spots': event['spots'],
+                             'signup_count': event['signup_count'],
+                             'time_start': event['time_start']})
+        return response
 
     def get_signups_for_event(self, event_id):
         """ Fetch the list of participants for a specific event """
-        _filter = '/eventsignups?where={"event":"%s"}' % event_id
+        _filter = ('/eventsignups?where={"event":"%s"}&embedded={"user":1}'
+                   % event_id)
         r = requests.get(self.api_url + _filter, auth=self.auth_obj)
+        if min(r.json()['total'], r.json()['max_results']) is 0:
+            raise Exception("No eventsignups found for this event")
+        response = list()
+        for key, eventsignup in r.json().iteritems():
+            user_info = eventsignup['user']
+            response.append({
+                             'firstname': user_info['firstname'],
+                             'lastname': user_info['lastname'],
+                             'nethz': user_info['nethz'],
+                             'email': user_info['email'],
+                             'checked_in': eventsignup['checked_in'],
+                             'legi': user_info['legi'],
+                             '_id': user_info['_id']})
         return r.json()
 
     def set_event(self, event_id):
         """ Set the event_id for this instance of the class """
         self.event_id = event_id
 
-    def _get_userid_from_legi(self, legi):
+    def _get_userinfo_from_userid(self, u_id):
+        """ Fetch the userinfos from the user_id """
+        r = requests.get(self.api_url + '/users/' + u_id)
+        return r.json()
+
+    def _get_userinfo_from_legi(self, legi):
         """ Fetch the user_id from a legi number """
         _filter = '/users?where={"legi":"%s"}' % legi
         r = requests.get(self.api_url + _filter,
                          auth=self.auth_obj)
         if r.status_code is 200:
-            return r.json()['_id']
-        return "Error: No user found"
+            return r.json()
+        raise Exception("Error: No user found")
 
-    def _get_userid_from_nethz(self, nethz):
+    def _get_userinfo_from_nethz(self, nethz):
         """ Fetch the user_id from a nethz username """
         _filter = '/users?where={"nethz":"%s"}' % nethz
         r = requests.get(self.api_url + _filter,
                          auth=self.auth_obj)
         if r.status_code is 200:
-            return r.json()['_id']
-        return "Error: No user found"
+            return r.json()
+        raise Exception("Error: No user found")
 
-    def _get_userid_from_email(self, email):
+    def _get_userinfo_from_email(self, email):
         """ Fetch the user_id from a mail adress """
         _filter = '/users?where={"email":"%s"}' % email
         r = requests.get(self.api_url + _filter,
                          auth=self.auth_obj)
         if r.status_code is 200:
             if len(r.json().keys()) is 1:
-                return r.json()['_id']
-        return "Error: Multiple entries found or no user found"
+                return r.json()
+        raise Exception("Error: Multiple entries found or no user found")
 
-    def _get_userid_from_info(self, info):
+    def _get_userinfo_from_info(self, info):
         """ Choose the function to use to fetch the u_id """
         if '@' in info:
-            user_id = self._get_userid_from_email(info)
+            user_info = self._get_userinfo_from_email(info)
         elif info.isalpha():
-            user_id = self._get_userid_from_nethz(info)
+            user_info = self._get_userinfo_from_nethz(info)
         else:
             if info[0] is 'S':
                 info.replace('S', '')
-            user_id = self._get_userid_from_legi(info)
-        return user_id
+            user_info = self._get_userinfo_from_legi(info)
+        return user_info
 
     def checkin_field(self, info):
         """ Check in a user to an event by flipping the checked_in value """
-        user_id = self._get_userid_from_info(info)
-        if 'Error' in user_id:
-            return False
+        user_id = self._get_userid_from_info(info)['_id']
         _filter = '?where={"user":"%s", "event":"%s"}' % user_id, self.event_id
         r = requests.get(self.api_url + '/eventsignups' + _filter,
                          auth=self.auth_obj)
         if r.status_code != 200:
-            return False
+            raise Exception("Error: Could not find eventsignup entry")
+        if r.json()['checked_in'] is True:
+            raise Exception("Error: User already checked in")
         etag = r.json()['_etag']
         url = self.api_url + '/eventsignups'
         header = {'If-Match': etag}
@@ -101,13 +141,13 @@ class AMIV_API_Interface:
     def checkout_field(self, info):
         """ Check in a user to an event by flipping the checked_in value """
         user_id = self._get_userid_from_info(info)
-        if 'Error' in user_id:
-            return False
         _filter = '?where={"user":"%s", "event":"%s"}' % user_id, self.event_id
         r = requests.get(self.api_url + '/eventsignups' + _filter,
                          auth=self.auth_obj)
         if r.status_code != 200:
-            return False
+            raise Exception("Error: Could not find the eventsignup entry")
+        if r.json()['checked_in'] is False:
+            raise Exception("Error: User already checked out or absent")
         etag = r.json()['_etag']
         url = self.api_url + '/eventsignups'
         header = {'If-Match': etag}
