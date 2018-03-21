d80333a6e85324d9b35eb67502a211e2a75dc5f3
Roger Barton <roger-barton@live.co.uk>
2018-03-21 13:35:47 +0100

    Changed local_run

--- run_local.py
+++ run_local.py
@@ -5,7 +5,14 @@ import os
 from app import create_app
 
 config_name = os.getenv('FLASK_CONFIG')
+if config_name is None:
+    config_name = 'development'
 app = create_app(config_name)
 
+#Use this to only run the app on this device, it cannot be accessed over the network
+#if __name__ == '__main__':
+#    app.run()
+
+#Use this to run the app so it can be accessed externally
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
