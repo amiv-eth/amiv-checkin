8d7399e6ba5d06ca71eb33ed2a5bd12d74911574
Sandro Lutz <code@temparus.ch>
2018-05-05 18:55:32 +0200

    Update deploy script

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     """
 
     # Put any configurations here that are common across all environments
-    AMIV_API_URL = "https://amiv-api.ethz.ch"
+    AMIV_API_URL = "https://api-dev.amiv.ethz.ch"
     AMIV_AUTH_GROUP_ID = "5a4ea42c10d8cf0015915151"
 
 
